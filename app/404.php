f14ffa186c7f68de8e63258de12a50d2a11a291a
CrazzyReeZzy <crazzyhappyend@gmail.com>
2019-04-03 23:53:59 +0500

    Создание в базы данных

---
+++ app/404.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sensor</title>
+    <meta name="description" content="sensor">
+    <link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="img/favicon/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png">
+    <link rel="stylesheet" href="css/normalize.css">
+</head>
+<body>
+    Ошибка 404 
+</body>
+    <!--script !-->
+    <script src="libs/jquery/jquery-1.11.2.min.js"></script>
+</html>
